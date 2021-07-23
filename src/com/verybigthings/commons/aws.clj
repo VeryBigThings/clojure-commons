d9b80ca64f357a1cc71eda614300ed99e00b63e5
Davor Visnic <davor.visnic@verybigthings.com>
2021-07-23 10:25:28 +0200

    Support AWS S3 presigned URL

--- src/com/verybigthings/commons/aws.clj
+++ src/com/verybigthings/commons/aws.clj
@@ -1,8 +1,14 @@
 (ns com.verybigthings.commons.aws
   (:require [clojure.string :as s]
-            [buddy.core.keys :as bk])
+            [buddy.core.keys :as bk]
+            [tick.alpha.api :as t])
   (:import (java.util Base64)
-           (java.security Signature SecureRandom)))
+           (java.security Signature SecureRandom)
+           (com.amazonaws HttpMethod)
+           (com.amazonaws.regions Regions)
+           (com.amazonaws.services.s3 AmazonS3ClientBuilder)
+           (com.amazonaws.auth BasicAWSCredentials AWSStaticCredentialsProvider)
+           (com.amazonaws.services.s3.model GeneratePresignedUrlRequest)))
 
 (defn- cloudfront-policy [hostname epoch-expiry-timestamp]
   (str "{\"Statement\":[{\"Condition\":{\"DateLessThan\":{\"AWS:EpochTime\":" epoch-expiry-timestamp "}},\"Resource\":\"https://" hostname "/*\"}]}"))
@@ -23,3 +29,20 @@
     {:cloudfront-key-pair-id cdn-access-key-id
      :cloudfront-policy (safe-base64-encode (.getBytes policy))
      :cloudfront-signature (sign cdn-private-key policy)}))
+
+(defn presigned-url
+  ([access-key-id secret-access-key region bucket path duration]
+   (presigned-url access-key-id secret-access-key region bucket path duration :put))
+  ([access-key-id secret-access-key region bucket path duration method]
+   (let [region (Regions/fromName region)
+         method (if (= method :put) HttpMethod/PUT HttpMethod/GET)
+         credentials (BasicAWSCredentials. access-key-id secret-access-key)
+         s3-client (-> (AmazonS3ClientBuilder/standard)
+                       (.withRegion region)
+                       (.withCredentials (AWSStaticCredentialsProvider. credentials))
+                       .build)
+         expiration (t/inst (t/>> (t/now) (t/new-duration duration :minutes)))
+         generate-presigned-url-request (doto (GeneratePresignedUrlRequest. bucket path)
+                                          (.setMethod method)
+                                          (.setExpiration expiration))]
+     (.toString (.generatePresignedUrl s3-client generate-presigned-url-request)))))
--- test/com/verybigthings/commons/aws_test.clj
+++ test/com/verybigthings/commons/aws_test.clj
@@ -1,6 +1,7 @@
 (ns com.verybigthings.commons.aws-test
   (:require [clojure.test :refer :all]
-            [com.verybigthings.commons.aws :refer [cdn-cookies]]
+            [clojure.string :refer [includes? starts-with?]]
+            [com.verybigthings.commons.aws :refer [cdn-cookies presigned-url]]
             [buddy.core.keys :as bk])
   (:import (java.util Base64)
            (java.security Signature)))
@@ -10,7 +11,10 @@
                 :cdn-access-key-id "APK1234567KPA7654321"
                 :cdn-private-key "-----BEGIN RSA PRIVATE KEY-----\nMIICWgIBAAKBgH71nWq/7v3t+ZuHS0ZwZ9n4b52VQfZHUiIxM7VdVNs5TeW6wfPw5KcHiEhOy/xjXbU5bdbM8S9wuiHj8iRgmvUnEtFZW8fN6/5uBuIA99JhAaTqqSmn0jy1uDwURa66/7cjgYCmdDZEft8NWUR8o3EZlBIPRdUg9AQpkIZlYnwlAgMBAAECgYBeH6Pb/32kfmh+WLiC+VTU8W12a7CicE/BD68hN5dwi7Rnq6SdQKuT9W6hBOIQNG7Fq1T2Waw5mp4z//WF6KUYKHtNsvv/xqHjIdCOKnDYX8oMsOuyK2d8ag7LUVqO6zNMjenFkh4WyszxiKoOmWMZsVg2llN8ei8PJdE6nYlnBQJBANwo4nLBsoJv9sUJdY/4ZncZSRhipTk6tMZEjbJJwgBiy+H9YzPf6NS0wzp0yEQhrEypT+/3Yi6w37F3vWvkvksCQQCToKBFgGoUIvB5J75V/pxxM0Doc0FLj5IV7fcPqH7bnb9atdG77Jaw1d9UD84DRYNhJGaA+xyjY6kNFAZj3WlPAkB6kyjAy6cfiEbUHRgfOCWUWbegpAtpeY38dV6OzRH+NQNepzz8PMXc52dqvGpjxHuxkvK2n/1CEaf5nrkPwSNRAkAdTTSClwp3FLhGU+0jhHXowhOQD9BV/xkv4Ru4r+j+DkF877nBYyRF1S2OsdRiMdnx3he6Yh09EPrs7ZyXFcthAkABkMmzn9Hxs80EKKYOghtqhtIs8nPqqvh0FpDolQL6TM2rEIMPqzZTilXl8cbFKHqeP4TMHmpx2rr0A/sg7V6u\n-----END RSA PRIVATE KEY-----"
                 :cdn-public-key "-----BEGIN PUBLIC KEY-----\nMIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgH71nWq/7v3t+ZuHS0ZwZ9n4b52VQfZHUiIxM7VdVNs5TeW6wfPw5KcHiEhOy/xjXbU5bdbM8S9wuiHj8iRgmvUnEtFZW8fN6/5uBuIA99JhAaTqqSmn0jy1uDwURa66/7cjgYCmdDZEft8NWUR8o3EZlBIPRdUg9AQpkIZlYnwlAgMBAAE=\n-----END PUBLIC KEY-----"
-                :cloudfront-policy "{\"Statement\":[{\"Condition\":{\"DateLessThan\":{\"AWS:EpochTime\":1908873209}},\"Resource\":\"https://www.verybigthings.com/*\"}]}"})
+                :cloudfront-policy "{\"Statement\":[{\"Condition\":{\"DateLessThan\":{\"AWS:EpochTime\":1908873209}},\"Resource\":\"https://www.verybigthings.com/*\"}]}"
+                :access-key-id "APK1234567KPA7654321"
+                :secret-access-key "4524b08bd853463fa0aa0f75e3c67a0a"
+                :aws-region "us-east-1"})
 
 (deftest test-cdn-cookies
   (testing "Generate AWS CloudFront cookies"
@@ -27,3 +31,18 @@
       (is (= (:cloudfront-key-pair-id cookies) (:cdn-access-key-id mock-data)))
       (is (= policy-decoded (:cloudfront-policy mock-data)))
       (is (.verify cipher signature-decoded)))))
+
+(deftest test-presigned-url
+  (testing "Generate AWS S3 presigned URL"
+    (let [access-key-id (:access-key-id mock-data)
+          region (:aws-region mock-data)
+          bucket (:hostname mock-data)
+          path "test/test.jpg"
+          expires 30
+          url (presigned-url access-key-id (:secret-access-key mock-data) region
+                bucket path expires)
+          expected-url (str "https://" bucket ".s3.amazonaws.com/" path)]
+      (is (starts-with? url expected-url))
+      (is (includes? url region))
+      (is (includes? url (str "X-Amz-Expires=" (* expires 60))))
+      (is (includes? url (str "X-Amz-Credential=" access-key-id))))))
